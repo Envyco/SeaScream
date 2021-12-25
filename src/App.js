6a117406fffea8a68315d9b761ac08f102fa251c
Envyco <96353392+Envyco@users.noreply.github.com>
2021-12-25 14:21:31 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -66,20 +66,7 @@ export const ResponsiveWrapper = styled.div`
 `;
 
 
-export const StyledImg = styled.img`
-  box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
-  background-color: var(--accent);
-  border-radius: 100%;
-  width: 200px;
-  @media (min-width: 900px) {
-    width: 250px;
-  }
-  @media (min-width: 1000px) {
-    width: 300px;
-  }
-  transition: width 0.5s;
-`;
+
 
 export const StyledLink = styled.a`
   color: var(--secondary);
